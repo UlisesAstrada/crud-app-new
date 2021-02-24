12484c73c89fcd8f698f235bb8772a539de5ed3c
Ulises <ulises_astrada@outlook.com>
2021-02-24 00:39:11 -0300

    Adding a form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
 import ListadoNombres from './components/ListadoNombres';
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <ListadoNombres />
     </div>
   );
--- src/components/ListadoNombres.jsx
+++ src/components/ListadoNombres.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import uniqid from 'uniqid'
 
 function ListadoNombres() {
+
+  const [nombre, setNombre] = useState('')
+  const [listadoNombres, setListadoNombres] = useState([])
+
+  const addNombre = () => {
+
+  }
+
   return (
     <div>
-            
+      <h2>Aplicación CRUD básica</h2>
+      <div className="col">
+        <h2>Listado de nombres</h2>
+      </div>
+      <div className="col">
+        <h2>Formulario para añadir nombres</h2>
+        <form className="form-group">
+          <input 
+          onChange= {(event) =>{setNombre(event.target.value)}} 
+          type="text" 
+          className="form-control mb-3" 
+          placeholder="Introduce el nombre"/>
+          <input 
+          className="btn btn-info btn-block" 
+          type="submit" 
+          value="Registrar nombre"/>
+          </form>
+      </div>    
     </div>
   )
 }
