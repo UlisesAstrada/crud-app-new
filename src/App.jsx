bdd5e066e5fda004fbcc4fb6c769ebfb2039b4b2
Ulises <ulises_astrada@outlook.com>
2021-02-24 00:21:17 -0300

    A crud app with React+Firebase

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import ListadoNombres from './components/ListadoNombres';
+
+function App() {
+  return (
+    <div className="App">
+      <ListadoNombres />
+    </div>
+  );
+}
+
+export default App;
